f73cd31f0b63853584138d98021e37c244dfb998
Matheus Tanaka <matheustanakanog@gmail.com>
2021-12-07 19:14:52 -0300

    feat: learning about Components and JSX

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+const App = () => {
+  const messages = ["hello world!", "olá mundo"];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {messages.map(message => <h1>{message}</h1>)}
     </div>
   );
 }
