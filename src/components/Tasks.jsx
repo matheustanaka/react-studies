3045acd641c1611c7621d759fb7ab01a67401693
Matheus Tanaka <matheustanakanog@gmail.com>
2021-12-15 20:37:31 -0300

    feat: render tasks with TaskItem component

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -3,6 +3,8 @@ import axios from 'axios';
 
 import './Tasks.scss';
 
+import TaskItem from './TaskItem'
+
 const Tasks = () => {
     
   const [tasks, setTasks] = useState([
@@ -29,14 +31,20 @@ const Tasks = () => {
             <div className="last-tasks">
             <h3>Últimas Tarefas</h3>
                 <div className="tasks-list">
-                    {tasks.filter(task => task.isCompleted === false).map(lastTask => <p>{lastTask.description}</p>)}
+                    {tasks.filter(task => task.isCompleted === false)
+                    .map((lastTask) => (
+                    <TaskItem task={lastTask}/> 
+                    ))}
                 </div>
             </div>
 
             <div className="completed-tasks">
             <h3>Tarefas Concluídas</h3>
                 <div className="tasks-list">
-                    {tasks.filter((task) => task.isCompleted).map((completedTask) => (<p>{completedTask.description}</p>))}
+                    {tasks.filter((task) => task.isCompleted)
+                    .map((completedTask) => (
+                    <TaskItem task={completedTask}/> 
+                    ))}
                 </div>
             </div>
         </div>
