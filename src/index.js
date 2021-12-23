c0c9443f53b504d842441273ce7e8200bd88f097
Matheus Tanaka <matheustanakanog@gmail.com>
2021-12-22 22:01:14 -0300

    build: add react-alert and react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { positions, transitions, Provider as AlertProvider } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
+import { BrowserRouter } from "react-router-dom";
 
-import './index.scss';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./index.scss";
+import reportWebVitals from "./reportWebVitals";
+
+import App from "./App";
+
+// optional configuration
+const options = {
+    // you can also just use 'bottom center'
+    position: positions.BOTTOM_CENTER,
+    timeout: 2500,
+    offset: "30px",
+    // you can also just use 'scale'
+    transition: transitions.SCALE,
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>, document.getElementById('root')
+    <React.StrictMode>
+        <BrowserRouter>
+            <AlertProvider template={AlertTemplate} {...options}>
+                <App />
+            </AlertProvider>
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
