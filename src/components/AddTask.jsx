7465660ed3e700192b6493ec666a1920aa6179f9
Matheus Tanaka <matheustanakanog@gmail.com>
2021-12-22 21:48:26 -0300

    feat: fetching and add tasks

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,15 +1,43 @@
-
 import { useState } from "react";
+import { FaPlus } from "react-icons/fa";
+import axios from "axios";
+import { useAlert } from "react-alert";
 
 import CustomInput from "./CustomInput";
+import CustomButton from "./CustomButton";
 
 import "./AddTask.scss";
 
-const AddTask = () => {
+const AddTask = ({ fetchTasks }) => {
     const [task, setTask] = useState("");
+
+    const alert = useAlert();
 
     const onChange = (e) => {
         setTask(e.target.value);
+    };
+
+    const handleTaskAddition = async () => {
+        try {
+            if (task.length === 0) {
+                return alert.error(
+                    "A tarefa precisa de uma descrição para ser adicionada."
+                );
+            }
+
+            await axios.post(`${process.env.REACT_APP_API_URL}/tasks`, {
+                description: task,
+                isCompleted: false,
+            });
+
+            await fetchTasks();
+
+            setTask("");
+
+            await alert.success("A tarefa foi adicionada com sucesso!");
+        } catch (_e) {
+            alert.error("Algo deu errado.");
+        }
     };
 
     return (
@@ -18,7 +46,11 @@ const AddTask = () => {
                 label="Adicionar tarefa..."
                 value={task}
                 onChange={onChange}
+                onEnterPress={handleTaskAddition}
             />
+            <CustomButton onClick={handleTaskAddition}>
+                <FaPlus size={14} color="#ffffff" />
+            </CustomButton>
         </div>
     );
 };
